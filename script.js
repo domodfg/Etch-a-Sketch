aeca0f67e7b200d5f6d7928a561ac072769c2ea4
domodfg <domodfg@yahoo.com.hk>
2022-07-07 19:17:42 +0800

    Add function for changing number of squares per side

--- script.js
+++ script.js
@@ -1,24 +1,56 @@
-let num =16;
-let squaredNum= num**2;
-const container=document.querySelector('.container');
-let squareWidth= 100/num;
+let num = 16;
+let squaredNum = num ** 2;
+const container = document.querySelector(".container");
+let squareWidth = 100 / num;
 
-
-for (let i=0; i<squaredNum; i++) {
-  const canvas=document.createElement('div');
+for (let i = 0; i < squaredNum; i++) {
+  const canvas = document.createElement("div");
   canvas.style.width = squareWidth + "%";
-  canvas.classList.add('canvas');
-  canvas.addEventListener('mouseover', () => {
-  canvas.classList.add ('colored');
+  canvas.classList.add("canvas");
+  canvas.addEventListener("mouseover", () => {
+    canvas.classList.add("colored");
   });
   container.appendChild(canvas);
 }
 
-const clear = document.querySelector('.clear');
-const allGrid = document.querySelectorAll('.canvas');
+const clear = document.querySelector(".clear");
+const numOfGrid = document.querySelector(".number");
+
 function clearColor() {
-  allGrid.forEach(function(item) {
-  item.classList.remove('colored');
+  const allGrid = document.querySelectorAll(".canvas");
+  allGrid.forEach(function (item) {
+    item.classList.remove("colored");
   });
 }
-clear.addEventListener('click', clearColor)
+
+clear.addEventListener("click", clearColor);
+
+function clearGrid() {
+  const allGrid = document.querySelectorAll(".canvas");
+  allGrid.forEach(function (item) {
+    container.removeChild(item);
+  });
+}
+
+function changeNumberOfGrid() {
+  let grid = prompt("Input the number of squares per side (1-100)");
+  if (grid > 100) {
+    alert("Out of range!");
+    return;
+  }
+  num = grid;
+  squaredNum = num ** 2;
+  squareWidth = 100 / num;
+  clearGrid();
+  for (let i = 0; i < squaredNum; i++) {
+    const canvas = document.createElement("div");
+    canvas.style.width = squareWidth + "%";
+    canvas.classList.add("canvas");
+    canvas.addEventListener("mouseover", () => {
+      canvas.classList.add("colored");
+    });
+    container.appendChild(canvas);
+  }
+}
+
+numOfGrid.addEventListener("click", changeNumberOfGrid);
