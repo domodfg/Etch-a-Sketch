4d5029f9048f968c9cea1de6ec7329bd06ba0a57
domodfg <domodfg@yahoo.com.hk>
2022-07-07 17:57:05 +0800

    Add mouseover eventlistener to the squares

--- script.js
+++ script.js
@@ -3,9 +3,15 @@ let squaredNum= num**2;
 const container=document.querySelector('.container');
 let squareWidth= 100/num;
 
-for (i=0; i<squaredNum; i++) {
+
+for (let i=0; i<squaredNum; i++) {
   const canvas=document.createElement('div');
   canvas.style.width = squareWidth + "%";
   canvas.classList.add('canvas');
+  canvas.addEventListener('mouseover', () => {
+  canvas.classList.add ('colored');
+  });
   container.appendChild(canvas);
 }
+
+
